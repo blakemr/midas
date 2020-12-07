802521226e0108741378331e686f1acfdf460659
Matthew Blake <mblak0625@gmail.com>
2020-12-07 12:21:11 -0500

    Turned the cursor back on, and it follows the proper position (sorta).

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ mod cli;
 use cli::Mode;
 use std::io;
 use std::io::{stdin, stdout, Write};
-use termion::cursor::DetectCursorPos;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::{clear, cursor};
@@ -15,16 +14,14 @@ fn main() -> io::Result<()> {
     let mut termout = stdout().into_raw_mode()?;
     let mut text = String::new();
     let mut command = String::new();
-    let mut c_pos: (u16, u16);
 
-    // Landing message, and hide the cursor
+    // Landing message
     write!(
         termout,
-        "{}{}<Esc> to exit. Type stuff, use alt, etc.{}{}",
+        "{}{}<Esc> to exit. Type stuff, use alt, etc.{}",
         clear::All,
         cursor::Goto(1, 1),
-        "\r",
-        cursor::Hide
+        "\r"
     )?;
 
     // Flush to screen
@@ -38,14 +35,9 @@ fn main() -> io::Result<()> {
             }
         }
 
-        c_pos = termout.cursor_pos()?;
-        write!(termout, "{}{}{}", clear::All, text, cursor::Goto(1, 1))?;
+        write!(termout, "{}{}{}", clear::All, cursor::Goto(1, 1), text)?;
         termout.flush()?;
-        write!(termout, "{}", cursor::Goto(c_pos.0, c_pos.1));
     }
-
-    // Turn the cursor back on
-    write!(termout, "{}", cursor::Show)?;
 
     Ok(())
 }
