5e4b320a45b7fe02c67fd472bc8d4b4a805d5b10
Matthew Blake <mblak0625@gmail.com>
2020-12-07 10:57:39 -0500

    Now can write lines, and insert mode support.

---
+++ src/cli.rs
@@ -0,0 +1,73 @@
+//! # Manage the CLI version of the text editor
+//!
+//! This crate is called when midas is run in terminal editor mode.
+use termion::event::{Event, Key};
+
+/// Sets up the modes of operation. These are named after the standards
+/// set by vim, but may be changed slightly since the modal commands are intended
+/// to work with a gui from the ground up.
+pub enum Mode {
+    Normal,
+    Insert,
+    Command,
+    Exit,
+}
+
+impl Mode {
+    /// Handles an input event
+    ///
+    /// Routes to different handle events based on current mode
+    ///
+    /// args:
+    ///     * event: Event - event to handle
+    ///     * command: &mut String - Command buffer. Stored until command is entered
+    ///     * text: &mut String - text to be edited
+    ///
+    /// TODO:
+    ///     * Normal Mode
+    ///     * Command Mode
+    ///     * change text's type?
+    pub fn handle(self, event: Event, command: &mut String, text: &mut String) -> Self {
+        match self {
+            Mode::Normal => Mode::Normal,
+            Mode::Insert => Mode::handle_insert(event, text),
+            Mode::Command => Mode::Command,
+            Mode::Exit => panic!("Unhandled Exit mode."),
+        }
+    }
+
+    /// Handles events in insert mode
+    fn handle_insert(event: Event, text: &mut String) -> Self {
+        match event {
+            Event::Key(Key::Esc) => {
+                return Mode::Exit;
+            }
+            Event::Key(Key::Char(c)) => {
+                text.push(c);
+            }
+            Event::Key(Key::Alt(c)) => {
+                text.push_str("Alt-");
+                text.push(c);
+            }
+            Event::Key(Key::Ctrl(c)) => {
+                text.push_str("Ctrl-");
+                text.push(c);
+            }
+            Event::Key(Key::Left) => {
+                text.push_str("<Left>");
+            }
+            Event::Key(Key::Right) => {
+                text.push_str("<Right>");
+            }
+            Event::Key(Key::Up) => {
+                text.push_str("<Up>");
+            }
+            Event::Key(Key::Down) => {
+                text.push_str("<Down>");
+            }
+            _ => {}
+        }
+
+        Mode::Insert
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+
+use cli::Mode;
+use std::io;
+use std::io::{stdin, stdout, Write};
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::{clear, cursor};
+
+fn main() -> io::Result<()> {
+    let mut mode = Mode::Insert;
+    let stdin = stdin();
+    let mut events = stdin.events();
+    let mut termout = stdout().into_raw_mode()?;
+    let mut text = String::new();
+    let mut command = String::new();
+
+    // Landing message, and hide the cursor
+    write!(
+        termout,
+        "{}{}<Esc> to exit. Type stuff, use alt, etc.{}{}{}",
+        clear::All,
+        cursor::Goto(1, 1),
+        cursor::Down(1),
+        "\r",
+        cursor::Hide
+    )?;
+
+    // Flush to screen
+    termout.flush()?;
+
+    loop {
+        if let Some(event) = events.next() {
+            mode = match mode.handle(event?, &mut command, &mut text) {
+                Mode::Exit => break,
+                mode => mode,
+            }
+        }
+
+        write!(termout, "{}{}", text, cursor::Goto(1, 2))?;
+        termout.flush()?;
+    }
+
+    // Turn the cursor back on
+    write!(termout, "{}", cursor::Show)?;
+
+    Ok(())
 }
