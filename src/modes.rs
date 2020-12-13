9e99f14c0c6cb2b05733e786672a08cfffae5a6b
Matthew Blake <mblak0625@gmail.com>
2020-12-13 13:38:10 -0500

    removed command mode. Can add later if needed.

--- src/modes.rs
+++ src/modes.rs
@@ -11,6 +11,86 @@ use termion::event::{Event, Key};
 pub enum Mode {
     Normal,
     Insert,
-    Command,
     Exit,
+}
+
+impl Mode {
+    /// Handles an input event
+    ///
+    /// Routes to different handle events based on current mode
+    ///
+    /// args:
+    ///     * event: Event - event to handle
+    ///     * command: &mut String - Command buffer. Stored until command is entered
+    ///     * text: &mut String - text to be edited
+    ///
+    /// TODO:
+    ///     * Normal Mode
+    ///     * Command Mode
+    ///     * change text's type?
+    pub fn handle(self, event: Event, text: &mut Text) -> Self {
+        match self {
+            Mode::Normal => Mode::handle_normal(event, text),
+            Mode::Insert => Mode::handle_insert(event, text),
+            Mode::Exit => panic!("Unhandled Exit mode."),
+        }
+    }
+
+    /// Handles events in insert mode
+    fn handle_insert(event: Event, text: &mut Text) -> Self {
+        match event {
+            Event::Key(Key::Esc) => {
+                return Mode::Normal;
+            }
+            Event::Key(Key::Char(c)) => {
+                text.push(c);
+            }
+            Event::Key(Key::Backspace) => {
+                text.pop();
+            }
+            // These are placeholder commands
+            Event::Key(Key::Alt(c)) => {
+                text.push_str("Alt-");
+                text.push(c);
+            }
+            Event::Key(Key::Ctrl(c)) => {
+                text.push_str("Ctrl-");
+                text.push(c);
+            }
+            Event::Key(Key::Left) => {
+                text.push_str("<Left>");
+            }
+            Event::Key(Key::Right) => {
+                text.push_str("<Right>");
+            }
+            Event::Key(Key::Up) => {
+                text.push_str("<Up>");
+            }
+            Event::Key(Key::Down) => {
+                text.push_str("<Down>");
+            }
+            _ => {}
+        }
+
+        Mode::Insert
+    }
+
+    // Handles events in normal modecursor: &mut Cursor
+    fn handle_normal(event: Event, text: &mut Text) -> Self {
+        match event {
+            Event::Key(Key::Char('i')) => {
+                return Mode::Insert;
+            }
+            Event::Key(Key::Esc) => {
+                return Mode::Exit;
+            }
+            Event::Key(Key::Char('w')) => {
+                let mut file = File::create("foo.txt").unwrap();
+                write!(file, "{}", text);
+            }
+            _ => {}
+        }
+
+        Mode::Normal
+    }
 }
